19a03c2e44d35105d6e8cbbefe02c63537181707
Naing Lu Tun <nainglu7924@gmail.com>
2024-07-19 18:05:10 +0630

    done with most part of the waitlist page, needs to implement controlled inputs

--- src/components/WaitListPage.jsx
+++ src/components/WaitListPage.jsx
@@ -2,10 +2,28 @@ import waitListPageBG from "../assets/bg-1.png"
 import "./css/WatiListPage.css"
 import starLogo from "../assets/star.svg"
 import halfStarLogo from "../assets/halfStar.svg"
+import { useEffect, useRef, useState } from "react"
 
 const WaitListPage = () => {
 
+
+    const [waitTimeUpTo3People, setWaitTimeUpTo3People] = useState("5-12 mins")
+    const [waitTimeUpTo6People, setWaitTimeUpTo6People] = useState("8-15 mins")
+
+    const[selectedPartySize, setSelectedPartySize] = useState(1)
+
+    const[firstName, setFirstName] = useState("")
+    const[lastName, setLastName] = useState("")
+    const[mobileNum, setMobileNum] = useState("")
+    const[email, setEmail] = useState("")
+    const[request, setRequest] = useState("")
+
+
     const selectedLocation = JSON.parse(localStorage.getItem("selected location"))
+
+    const handleSelectedPartySize= (partySize) => {
+        setSelectedPartySize(partySize)
+    }
 
     const fourStars = [1, 2, 3, 4]
     const RatingDisplay = () => (
@@ -16,6 +34,20 @@ const WaitListPage = () => {
             <img src={halfStarLogo} alt="half-rating-image" className="ratingStars"/>
         </div>
     )
+    const radioCount = [1, 2, 3, 4, 5, 6, "7+"]
+    const RadioDisplay = () => (
+        <div className="selectPartySizeContainer">
+            
+            {radioCount.map((radio, index) => (
+                <>
+                    <label key={`radioLabel-${radio}`} onClick={() => handleSelectedPartySize(radio)} className={`partySizeLabel ${selectedPartySize === radio? "activeRadio": ""}`} htmlFor={`radio-${radio}`}>{radio}</label>
+                    <input key={`radioInout-${radio}`} className="partySizeInput" type="radio" id={`radio-${radio}`}name="partySize"/>
+                </>
+                
+            ))}
+        </div>
+    )
+
 
     return (
         <div id="WaitListPage">
@@ -46,7 +78,40 @@ const WaitListPage = () => {
                 </div>
 
                 <div id="inputFieldContainer">
+                    <div id="waitListInputHeader">
+                        <h2 className="largeText">Join the Waitlist</h2>
+                        <div className="waitTimeDisplayContainer">
+                            <span className="liveText">
+                            Live 
+                            <span className="forPulsingAnimation"></span>
+                            </span>
+                            {selectedPartySize < 4 && 
+                            <h2 className="largeText waitTimeText">Wait time: <span className="tealText">{waitTimeUpTo3People}</span></h2>}
+                            
+                            {selectedPartySize >= 4 &&  <h2 className="largeText waitTimeText">Wait time: <span className="tealText">{waitTimeUpTo6People}</span></h2>
+                            }
 
+                            {selectedPartySize === "7+" && <h2 className="largeText waitTimeText">Wait time: <span className="redText">Unavailable</span></h2>}
+                        </div>
+
+                        <h2 className="radioHeader mediumText radioAndInfoHeader">Select your party size</h2>
+                        <RadioDisplay/>
+
+                        <div id="inputFieldSection">
+                            <h2 className="inputFieldHeader mediumText radioAndInfoHeader">Your Information</h2>
+                            <div className="firstNameAndLastNameInput twoInputDisplayContainer">
+                                <input type="text" placeholder="First name" className="smallText inputFields" value={firstName} onChange={e => setFirstName(e.target.value)}/>
+                                <input type="text" placeholder="Last name" className="smallText inputFields" value={lastName} onChange={e => setLastName(e.target.value)}/>
+                            </div>
+                            
+                            <div className="phNumAndEmailInput twoInputDisplayContainer">
+                                <input type="tel" placeholder="Moblie number" className="smallText inputFields" value={mobileNum} onChange={e => setMobileNum(e.target.value)}/>
+                                <input type="email" placeholder="Email" className="smallText inputFields" value={email} onChange={e => setEmail(e.target.value)} />
+                            </div>
+                            
+                            <input type="text" placeholder="Requests (Optional)" className="smallText inputFields" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
