7989b89fe448274ba51913157b51d64184c83538
Naing Lu Tun <nainglu7924@gmail.com>
2024-07-23 23:58:56 +0630

    added build to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/boiling-point/",
   plugins: [react()],
-})
+  build: {
+    outDir: 'build'  // Change output directory to 'build'
+  }
+});
